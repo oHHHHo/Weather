cc4b34499aa91682a0819f1da4b0f4b0d79d15f9
liyp <liyoupeng250@outlook.com>
2018-08-03 17:44:15 +0800

    Weather search city 2018-08-03

--- app/src/main/java/lyp/com/weather/SelectCity.java
+++ app/src/main/java/lyp/com/weather/SelectCity.java
@@ -8,15 +8,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 import lyp.com.weather.util.City;
-import lyp.com.weather.util.CityDB;
 
 /**
  * Created by liyp on 18-8-3.
@@ -26,9 +26,15 @@ public class SelectCity extends Activity implements View.OnClickListener {
 
     private ImageView backBtn;
     private ListView cityListLv;
+    private EditText searchEt;
+    private Button searchBt;
+    private boolean SARCH_FLAG;
+    private int checkedCityCode;
+
     private List<City> mCityList;
     private MyApplication myApplication;
-    private ArrayList<String> cityNameList;
+    private ArrayList<String> cityInfo, searchList;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,19 +42,25 @@ public class SelectCity extends Activity implements View.OnClickListener {
         setContentView(R.layout.select_city);
 
         backBtn = findViewById(R.id.title_selectCity_back);
+        searchEt = findViewById(R.id.selectcity_search);
+        searchBt = findViewById(R.id.selectcity_search_btn);
         backBtn.setOnClickListener(this);
+        searchBt.setOnClickListener(this);
 
         myApplication = (MyApplication) MyApplication.getInstance();
         mCityList = myApplication.getCityList();
-        cityNameList = new ArrayList<>();
+        cityInfo = new ArrayList<>();
         for (int i = 0; i < mCityList.size(); i++) {
+            String no_ = Integer.toString(i+1);
+            String number = mCityList.get(i).getNumber();
+            String province = mCityList.get(i).getProvince();
             String cityName = mCityList.get(i).getCity();
-            cityNameList.add(cityName);
+            cityInfo.add("NO." + no_ + ":" + number + "-" + province + "_" + cityName);
         }
 
         cityListLv = findViewById(R.id.selectcity_lv);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SelectCity.this,
-                android.R.layout.simple_list_item_1, cityNameList);
+        adapter = new ArrayAdapter<String>(SelectCity.this,
+                android.R.layout.simple_list_item_1, cityInfo);
         cityListLv.setAdapter(adapter);
 
         //添加ListView点击事件
@@ -57,9 +69,16 @@ public class SelectCity extends Activity implements View.OnClickListener {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 int updateCityCode = Integer.parseInt(mCityList.get(position).getNumber());
                 Log.d("update city code", Integer.toString(updateCityCode));
+                int code;
+                if (SARCH_FLAG) {
+                    code = checkedCityCode;
+                } else {
+                    code = updateCityCode;
+                }
                 Intent intent = new Intent();
-                intent.putExtra("result", updateCityCode);
+                intent.putExtra("result", code);
                 SelectCity.this.setResult(RESULT_OK, intent);
+                checkedCityCode = 0;
                 finish();
             }
         };
@@ -72,6 +91,26 @@ public class SelectCity extends Activity implements View.OnClickListener {
             case R.id.title_selectCity_back:
                 finish();
                 break;
+            case R.id.selectcity_search_btn:
+                SARCH_FLAG = true;
+                String city = searchEt.getText().toString();
+                Log.d("Search", city);
+                searchList = new ArrayList<>();
+                for (int i = 0; i < mCityList.size(); i++) {
+                    String cityName = mCityList.get(i).getCity();
+                    if (city != null && cityName.equals(city)) {
+                        String no_ = Integer.toString(i+1);
+                        String number = mCityList.get(i).getNumber();
+                        String province = mCityList.get(i).getProvince();
+                        searchList.add("NO." + no_ + ":" + number + "-" + province + "_" + cityName);
+                        checkedCityCode = Integer.parseInt(number);
+                        break;
+                    }
+                }
+                adapter = new ArrayAdapter<String>(SelectCity.this,
+                        android.R.layout.simple_list_item_1, searchList);
+                cityListLv.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
             default:
                 break;
         }
